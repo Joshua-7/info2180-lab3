683e59faa5191f1aa8795b54951a2cb98f7123f3
Errol Green <egreen3000@yahoo.com>
2021-10-10 03:31:11 -0500

    Added X and O functionality

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,21 +1,41 @@
+let tracker =[];
+
 (function(window, document, undefined){
     window.onload = init;
     
       function init()
       {
-        var set = document.getElementById("board");
+        let set = document.getElementById("board");
         for(var p =0;p<9;p++)
         {
-          set.children[p].setAttribute("class","square");  
+          set.children[p].setAttribute("class","square");
+          set.children[p].setAttribute("id",p);
+          set.children[p].addEventListener("click",function(){oclick(this.id,set)});
         }
       }
     
     })(window, document, undefined);
 
 
-
-function setup()
-{
-    var set = document.getElementById("board");
-    console.log(set);
+function oclick(id,hope)
+{   
+    console.log(tracker);
+    if (tracker.length == 0)
+    {
+        hope.children[id].textContent ="X";
+        hope.children[id].setAttribute("class","square.X");
+        tracker.push("X");
+    }
+    else if (tracker[tracker.length-1]=="X")
+    {
+        hope.children[id].textContent ="O";
+        hope.children[id].setAttribute("class","square.O");
+        tracker.push("O");
+    }
+    else if ((tracker[tracker.length-1]=="O"))
+    {
+        hope.children[id].textContent ="X";
+        hope.children[id].setAttribute("class","square.X");
+        tracker.push("X");
+    }
 }
