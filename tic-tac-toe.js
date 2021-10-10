c967055d69bf53634de6f3b2dbeef134949b1c95
Errol Green <egreen3000@yahoo.com>
2021-10-10 04:23:50 -0500

    Added hover style change

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -8,9 +8,11 @@ let tracker =[];
         let set = document.getElementById("board");
         for(var p =0;p<9;p++)
         {
-          set.children[p].setAttribute("class","square");
+          set.children[p].classList.add("square");
           set.children[p].setAttribute("id",p);
           set.children[p].addEventListener("click",function(){oclick(this.id,set)});
+          set.children[p].addEventListener("mouseover",function(){hover(this.id,set)});
+          set.children[p].addEventListener("mouseout",function(){out(this.id,set)});
         }
       }
     
@@ -19,23 +21,32 @@ let tracker =[];
 
 function oclick(id,hope)
 {   
-    console.log(tracker);
     if (tracker.length == 0)
     {
         hope.children[id].textContent ="X";
-        hope.children[id].setAttribute("class","square.X");
+        hope.children[id].classList.add("X");
         tracker.push("X");
     }
     else if (tracker[tracker.length-1]=="X")
     {
         hope.children[id].textContent ="O";
-        hope.children[id].setAttribute("class","square.O");
+        hope.children[id].classList.add("O");
         tracker.push("O");
     }
     else if ((tracker[tracker.length-1]=="O"))
     {
         hope.children[id].textContent ="X";
-        hope.children[id].setAttribute("class","square.X");
+        hope.children[id].classList.add("X");
         tracker.push("X");
     }
+}
+
+function hover(id,hope)
+{
+    hope.children[id].classList.add("hover");
+}
+
+function out(id,hope)
+{
+    hope.children[id].classList.remove("hover");
 }
